de61ee695b317538718d310c716632024fb1d5f5
MatheusBarbosa01 <mbdocarmo97@gmail.com>
2024-06-30 10:27:31 -0300

    adiciona detecção de faces

--- sistema_presença/reconhecimento.py
+++ sistema_presença/reconhecimento.py
@@ -44,3 +44,25 @@ Labels = np.asarray(Labels, dtype=np.int32)
 model = cv2.face.LBPHFaceRecognizer_create()
 model.train(Training_Data, Labels)
 print("TREINAMENTO EFETUADO")
+
+# Caminho do classificador em cascata
+cascade_path = r'L:\python-recognition-opencv-main\.venv\Lib\haarcascade_frontalface_default.xml'
+
+face_classifier = cv2.CascadeClassifier(cascade_path)
+
+# Verifica se o classificador em cascata foi carregado corretamente
+if face_classifier.empty():
+    print("Erro ao carregar o classificador em cascata.")
+    exit()
+
+def face_detector(img, size=0.5):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    if faces is ():
+        return img, []
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
+        roi = img[y:y + h, x:x + w]
+        roi = cv2.resize(roi, (200, 200))
+    return img, roi
+
